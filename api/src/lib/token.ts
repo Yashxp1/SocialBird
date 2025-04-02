04506c00ba65a1399dd49a6e06f5de66d891d915
Yash Raj <kyashraj991@gmail.com>
2025-04-02 10:52:07 +0530

    created jwt token function

---
+++ api/src/lib/token.ts
@@ -0,0 +1,14 @@
+import jwt from 'jsonwebtoken'
+import { Response } from 'express';
+
+const generateToken = (userId:string, res:Response) => {
+  const token = jwt.sign({userId}, process.env.JWT_SECRET as string, {
+    expiresIn: '7d',
+  })
+  
+  res.cookie('jwt', token, {httpOnly: true, secure: true,sameSite:'strict'})
+  res.status(200).json({message: 'Login successful'})
+
+  return token
+}
+
