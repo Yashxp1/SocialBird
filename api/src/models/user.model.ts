8f0d081ff24f3d3b3d4dbd84105a7e5da5ae32a6
Yash Raj <kyashraj991@gmail.com>
2025-04-02 10:30:17 +0530

    created user model

---
+++ api/src/models/user.model.ts
@@ -0,0 +1,37 @@
+import mongoose, { Schema, Document, model } from 'mongoose';
+
+interface UserType extends Document {
+  name: string;
+  username: string;
+  email: string;
+  password: string;
+}
+
+const userSchema = new Schema<UserType>({
+  name: {
+    type: String,
+    trim: true,
+    required : [true, "Name is required"],
+  },
+  username: {
+    type: String,
+    unique: [true, 'Username is already taken'],
+    trim: true,
+    required : [true, "Username is required"],
+    minlength: 3,
+  } ,
+  email: {
+    type: String,
+    unique: [true, 'Email is is already taken'],
+    trim: true,
+    required : [true, "Email is required"],
+    minlength: 3,
+  }, 
+  password: {
+    type:String,
+    required: true,
+  }
+});
+
+const User = mongoose.model('User', userSchema) 
+export default User
