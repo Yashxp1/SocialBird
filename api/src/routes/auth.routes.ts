3cb9e37aa26e79ac392454da9c5bf2f0fb450aed
Yash Raj <kyashraj991@gmail.com>
2025-04-03 00:31:04 +0530

    cloudinary setup

--- api/src/routes/auth.routes.ts
+++ api/src/routes/auth.routes.ts
@@ -1,11 +1,12 @@
 import express from 'express'
 import { login, logout, register, updateProfile } from '../controllers/user.controller'
+import { protectRoute } from '../middlewares/auth.middleware'
 
 const authRoute = express.Router()
 
 authRoute.post('/auth/register',  register)
 authRoute.post('/auth/login', login)
 authRoute.post('/auth/logout' , logout)
-authRoute.put('/auth/update-profile', updateProfile)
+authRoute.put('/auth/update-profile', protectRoute,updateProfile)
 
 export default authRoute
