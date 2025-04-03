a7e035a4b3be4e378287cf71d5d7b7fc628d2083
Yash Raj <kyashraj991@gmail.com>
2025-04-03 22:28:40 +0530

    added send message function

--- api/src/controllers/message.controller.ts
+++ api/src/controllers/message.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import User from '../models/user.model';
 import { Document } from 'mongoose';
 import Message from '../models/message.model';
+import cloudinary from '../lib/cloudinary';
 
 interface RequestWithUser extends Request {
   user?: Document<typeof User>;
@@ -55,6 +56,37 @@ export const getMessages = async (
     console.error('Error in getMessages', error);
     res.status(500).json({
       Error: 'Internal server error',
+    });
+  }
+};
+
+export const sendMessage = async (
+  req: RequestWithUser,
+  res: Response
+): Promise<void> => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user?._id;
+
+    let imageUrl;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    await newMessage.save();
+  } catch (error) {
+    console.log('Error in send message', error);
+    res.status(500).json({
+      message: 'Server Error',
     });
   }
 };
