70230584921f471c7fbc2d6d2ce083a138175d19
Yash Raj <kyashraj991@gmail.com>
2025-04-21 23:16:42 +0530

    Edit scollbar color

--- client/src/components/ChatContainer.tsx
+++ client/src/components/ChatContainer.tsx
@@ -34,10 +34,10 @@ const ChatContainer = () => {
   if (isMessageLoading) return <div>LOADING...</div>;
 
   return (
-    <div className="flex-1 flex flex-col overflow-auto">
+    <div className="flex-1 flex flex-col">
       <ChatHeader />
 
-      <div className="flex-1 overflow-y-auto p-4 space-y-0">
+      <div className="flex-1 overflow-auto p-4  space-y-0">
         {messages.map((message) => (
           <div
             ref={messageEndRef}
--- client/src/components/ChatHeader.tsx
+++ client/src/components/ChatHeader.tsx
@@ -1,3 +1,4 @@
+import { X } from 'lucide-react';
 import { useChatStore } from '../store/useChatStore';
 import { useAuthStore } from '../store/userAuthStore';
 
@@ -5,11 +6,11 @@ const ChatHeader = () => {
   const { selectedUser, setSelectedUser } = useChatStore();
   const { onlineUsers } = useAuthStore();
   return (
-    <div className="p-2.5  border-b border-blue-700">
+    <div className="p-2.5  border-2 rounded-md  border-gray-600">
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-3">
-          {/* Avatar */}
-          <div className="avatar">
+  
+          <div className="">
             <div className="size-10 rounded-full relative">
               <img
                 src={selectedUser?.profilePic || '/avatar.png'}
@@ -29,7 +30,7 @@ const ChatHeader = () => {
 
 
         <button onClick={() => setSelectedUser(null)}>
-          X
+        <X />
         </button>
       </div>
     </div>
