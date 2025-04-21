60aca1f5618d8fac92e7ef90deac7c0ff854eeb1
Yash Raj <kyashraj991@gmail.com>
2025-04-21 22:14:02 +0530

    UI tweaks

--- client/src/components/ChatContainer.tsx
+++ client/src/components/ChatContainer.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useChatStore } from '../store/useChatStore';
 import ChatHeader from './ChatHeader';
 import MessageInput from './MessageInput';
@@ -6,15 +6,30 @@ import { useAuthStore } from '../store/userAuthStore';
 import { formatMessageTime } from '../lib/timeFormat';
 
 const ChatContainer = () => {
-  const { messages, getMessages, isMessageLoading, selectedUser } =
-    useChatStore();
+  const {
+    messages,
+    getMessages,
+    isMessageLoading,
+    selectedUser,
+    subscribeToMessages,
+    unSubscribeToMessages,
+  } = useChatStore();
 
   const { authUser } = useAuthStore();
+  const messageEndRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     if (!selectedUser) return;
     getMessages(selectedUser._id);
+    subscribeToMessages();
+
+    return () => unSubscribeToMessages();
   }, [selectedUser?._id, getMessages]);
+
+  useEffect(() => {
+    if(messageEndRef.current && messages)
+      messageEndRef.current.scrollIntoView({behavior: "smooth"})
+  },[messages])
 
   if (isMessageLoading) return <div>LOADING...</div>;
 
@@ -25,28 +40,30 @@ const ChatContainer = () => {
       <div className="flex-1 overflow-y-auto p-4 space-y-0">
         {messages.map((message) => (
           <div
+            ref={messageEndRef}
             key={message._id}
             className={`chat ${
-              message.senderId === authUser?._id ? 'chat-end' : 'chat-start'
+              message.senderId === authUser?._id ? 'bg-purple-900 flex justify-end' : 'bg-blue-900'
             }`}
           >
-            <div className="chat-image avatar">
-              <div className="size-10 rouded-full border">
+            {/* <div className="chat-image avatar border-red-700 border">
+              <div className="size-10">
                 <img
                   src={
                     message.senderId === authUser?._id
                       ? authUser.profilePic || '/avatar.png'
                       : selectedUser?.profilePic || '/avatar.png'
                   }
+                   className='rounded-full border-red-500 border-2'
                 />
               </div>
-            </div>
-            <div className="chat-header mb-1">
+            </div> */}
+            <div className="mb-1">
               <time className="text-xs opacity-50 ml-1">
                 {formatMessageTime(message.createdAt)}
               </time>
             </div>
-            <div className="chat-bubble flex flex-col">
+            <div className="flex flex-col rounded-md">
               {message.image && (
                 <img
                   src={message.image}
@@ -54,12 +71,12 @@ const ChatContainer = () => {
                   className="sm:max-w-[200px] rounded-md mb-2"
                 />
               )}
-              {message.text && <p>{message.text}</p>}
+              {message.text && <p className='pl-2'>{message.text}</p>}
             </div>
           </div>
         ))}
       </div>
-      <p>Messages Loading...</p>
+
       <MessageInput />
     </div>
   );
