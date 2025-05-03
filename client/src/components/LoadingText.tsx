bcfcbc65a1053661ae07d780b550125f6e9e3d07
Yash Raj <kyashraj991@gmail.com>
2025-05-03 15:09:11 +0530

    bg ui fix

--- client/src/components/LoadingText.tsx
+++ client/src/components/LoadingText.tsx
@@ -1,27 +1,32 @@
 import { useState, useEffect } from 'react';
+import { ThemeProvider } from '../context/ThemeContext';
 
 export default function LoadingText() {
   const [dots, setDots] = useState('');
-  
+
   useEffect(() => {
     const interval = setInterval(() => {
-      setDots(prevDots => {
+      setDots((prevDots) => {
         if (prevDots.length >= 3) {
           return '';
         }
         return prevDots + '.';
       });
     }, 500);
-    
+
     return () => clearInterval(interval);
   }, []);
-  
+
   return (
-    <div className="flex flex-col items-center justify-center dark:bg-blue-100 bg-gray-900 p-8 bg-gray-100 rounded-lg shadow-md">
-      <div className="text-3xl font-bold text-blue-600">
-        Loading<span className="inline-block w-12">{dots}</span>
+    <ThemeProvider>
+      <div className="flex flex-col items-center justify-center dark:bg-blue-100 bg-gray-900 p-8 h-screen">
+        <div className="text-3xl font-bold text-blue-600">
+          Loading<span className="inline-block w-12">{dots}</span>
+        </div>
+        <div className="mt-4 text-gray-600">
+          Please wait while we process your request
+        </div>
       </div>
-      <div className="mt-4 text-gray-600">Please wait while we process your request</div>
-    </div>
+    </ThemeProvider>
   );
 }
